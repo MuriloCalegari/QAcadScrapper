f40024c3829ed45db68c42ef320536a092a6370a
Murilo Calegari <murilo.calegari.souza@gmail.com>
2019-06-01 13:18:00 -0300

    Tests

--- src/test/java/calegari/murilo/qacadtest/QAcadTest.java
+++ src/test/java/calegari/murilo/qacadtest/QAcadTest.java
@@ -8,27 +8,53 @@ import static org.junit.Assert.*;
 
 
 import javax.security.auth.login.LoginException;
-import java.net.ConnectException;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Properties;
 
 public class QAcadTest {
 
     @Test
-    public void testGetAllSubjects() {
-        QAcadScrapper qAcadScrapper = new QAcadScrapper("https://academico3.cefetes.br/qacademico");
+    public void testGetAllSubjects() throws IOException {
+
+        Properties p = new Properties();
+        p.load(new FileReader(new File("C:\\Users\\muril\\IdeaProjects\\QAcadScrapper\\src\\test\\resources\\keystore.properties")));
+
+        User user = new User(p.get("login").toString(),  p.get("password").toString());
+        QAcadScrapper qAcadScrapper = new QAcadScrapper("https://academico3.cefetes.br/qacademico", user);
 
         List<Subject> subjects = null;
         try {
-            User user = new User("USERNAME", "PASSWORD");
 
-            qAcadScrapper.loginToQAcad(user);
-
+            user.setOptimizeEncryptionEnabled(true);
+            user.setMultiThreadEnabled(true);
 
             subjects = qAcadScrapper.getAllSubjectsAndGrades();
-        } catch (ConnectException | LoginException e) {
+        } catch (IOException | LoginException e) {
             e.printStackTrace();
         }
 
         assertNotNull(subjects);
+    }
+
+    @Test
+    public void testEncryption() {
+        User user = new User("20171xxxxx0289", "xxxxxxxxxxx");
+
+        user.setOptimizeEncryptionEnabled(true);
+        user.setMultiThreadEnabled(true);
+        user.encryptFields("67e522132eedc63fc983e14a2b35335", "fd31baa1c64b1e6233345e4685b91e1");
+    }
+
+    @Test
+    public void testExtractDate() {
+        String dateText = "03/05/2019";
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(dateText, dateTimeFormatter);
+
+        assertNotNull(date);
     }
 }
